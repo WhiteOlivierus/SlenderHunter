13c5e2ae5af9967b75fee5aaab4726dd2b3798c5
WhiteOlivierus <36532695+WhiteOlivierus@users.noreply.github.com>
2018-07-25 14:37:36 +0200

    Slender Rotation
    The slender tracks the player

---
+++ Assets/Scripts/HitSlender.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitSlender : MonoBehaviour
+{
+    void Update()
+    {
+        //Mouse press
+        if (Input.GetMouseButton(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit rayHit;
+
+        //shoot a raycast
+        if (Physics.Raycast(transform.position, transform.forward, out rayHit))
+        {
+            GameObject hitObject = rayHit.collider.gameObject;
+            // if hit a slender return the hit and remove health
+            if (hitObject.name == "Slender")
+            {
+                hitObject.GetComponent<SlenderBehaviour>().ResetSlender();
+            }
+        }
+    }
+}
--- Assets/Scripts/SlenderBehaviour.cs
+++ Assets/Scripts/SlenderBehaviour.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class SlenderBehaviour : MonoBehaviour
 {
-    public GameObject slenderObject;
     public  int damage = 5;
     public float speed = 5.0f;
     public float minRespawnTime = 10f;
     public float respawnTimeBias = 10f;
     public float maxDistance = 90f;
 
+    private GameObject player;
     private int health = 100;
     private bool hit = false;
     private int stage = 0;
@@ -20,51 +20,27 @@ public class SlenderBehaviour : MonoBehaviour
         //The kickstart apperence for the slender
         float respawnTime = Random.Range(minRespawnTime, minRespawnTime + respawnTimeBias);
         StartCoroutine("WaitForRespawn", respawnTime);
+        player = GameObject.Find("FPSController");
 	}
 
     void Update()
     {
-        //Mouse press
-        if (Input.GetMouseButton(0))
-        {
-            //shoot a ray returns if hit slender
-            hit = Shoot();
-
-            if (hit)
-            {
-                //Reset the slender attack loop
-                stage = 0;
-                StopCoroutine("LifeSpan");
-                float respawnTime = Random.Range(minRespawnTime, minRespawnTime + respawnTimeBias);
-                StartCoroutine("WaitForRespawn", respawnTime);
-            }
-        }
-
         //Rotate the slender towards me
-        slenderObject.transform.LookAt(transform.position);
-    }
+        //Calculate the direction for the rotation
+        Vector3 direction = player.transform.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, 5 * Time.deltaTime);
 
-    private void Respawn()
-    {
-        //Find the location that the slender has to stand
-        PlaceSlender();
-
-        //show the slender
-        StartCoroutine("LifeSpan", speed);
+        Debug.DrawRay(transform.position,transform.forward*20, Color.blue);
     }
 
     private void PlaceSlender()
     {
-        //Reset trees -- for debugging only
-        List<Collider> resetTrees = new List<Collider>(Physics.OverlapSphere(transform.position, 200f, 1 << 8));
-        for (int l = 0; l < resetTrees.Count; l++)
-        {
-            resetTrees[l].GetComponent<Renderer>().material.color = new Color(0.26f, 0.17f,0.09f);
-        }
+        ResetTrees();
 
         //all the trees in range
-        List<Collider> stageOneSelected = new List<Collider>(Physics.OverlapSphere(transform.position, maxDistance - (20 * stage), 1 << 8));
-        List<Collider> stageTwoSelected = new List<Collider>(Physics.OverlapSphere(transform.position, maxDistance - 20, 1 << 8));
+        List<Collider> stageOneSelected = new List<Collider>(Physics.OverlapSphere(player.transform.position, maxDistance - (20 * stage), 1 << 8));
+        List<Collider> stageTwoSelected = new List<Collider>(Physics.OverlapSphere(player.transform.position, maxDistance - 20, 1 << 8));
 
         //Remove the trees from stage 2 range
         for (int i = 0; i < stageOneSelected.Count; i++)
@@ -86,31 +62,56 @@ public class SlenderBehaviour : MonoBehaviour
         selectedTree.GetComponent<Renderer>().material.color = Color.yellow; // -- for debugging only
 
         //Change location to slender
-        slenderObject.transform.position = selectedTree.position;
-        slenderObject.transform.localPosition -= slenderObject.transform.forward;
+        gameObject.transform.position = new Vector3(selectedTree.position.x, gameObject.transform.position.y, selectedTree.position.z);
+        gameObject.transform.localPosition -= gameObject.transform.forward;
+    }
+
+    public void ResetSlender()
+    {
+        //Reset the slender attack loop
+        stage = 0;
+        StopCoroutine("LifeSpan");
+        float respawnTime = Random.Range(minRespawnTime, minRespawnTime + respawnTimeBias);
+        StartCoroutine("WaitForRespawn", respawnTime);
+        print("slender was hit");
+    }
+
+    private void ResetTrees()
+    {
+        //Reset trees -- for debugging only
+        List<Collider> resetTrees = new List<Collider>(Physics.OverlapSphere(player.transform.position, 200f, 1 << 8));
+        for (int l = 0; l < resetTrees.Count; l++)
+        {
+            resetTrees[l].GetComponent<Renderer>().material.color = new Color(0.26f, 0.17f, 0.09f);
+        }
     }
 
     IEnumerator WaitForRespawn(float respawnTime)
     {
         //hide the slender for a reset attack loop
         print("Killed"); // -- for debugging only
-        slenderObject.SetActive(false);
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
         yield return new WaitForSecondsRealtime(respawnTime);
-        Respawn();
+
+        //Find the location that the slender has to stand
+        PlaceSlender();
+
+        //show the slender
+        StartCoroutine("LifeSpan", speed);
     }
 
     IEnumerator LifeSpan(float lifeTime)
     {
         //toggle the visibilty of the slender
-        if (!slenderObject.activeSelf)
+        if (!gameObject.GetComponent<MeshRenderer>().enabled)
         {
             print("Spawned"); // -- for debugging only
-            slenderObject.SetActive(true);
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
         }
         else
         {
             print("Hidden"); // -- for debugging only
-            slenderObject.SetActive(false);
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
         }
 
         //wait seconds before reappering or hidding
@@ -119,28 +120,14 @@ public class SlenderBehaviour : MonoBehaviour
         //if the slender is not hit it moves closer
         if(!hit)
         {
-            print(stage);
-            stage++;
-            Respawn();
+            //print(stage);
+            //stage++;
+
+            //Find the location that the slender has to stand
+            PlaceSlender();
+
+            //show the slender
+            StartCoroutine("LifeSpan", speed);
         }
-    }
-
-    private bool Shoot()
-    {
-        RaycastHit rayHit;
-
-        //shoot a raycast
-        if(Physics.Raycast(transform.position, transform.forward, out rayHit))
-        {
-
-            // if hit a slender return the hit and remove health
-            if(rayHit.collider.gameObject.name == "Slender")
-            {
-                health -= 5;
-                return true;
-            }
-        }
-
-        return false;
     }
 }
